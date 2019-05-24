30dbcbf9ced3e8c2eb8b6d3bbabff96b0f399f2b
shiva-banihashem <50642099+shiva-banihashem@users.noreply.github.com>
2019-05-23 23:36:03 -0500

    Update lists.js

--- lists.js
+++ lists.js
@@ -41,3 +41,29 @@ function arrayToList(arrayArg) {
                   
 
 console.log(arrayToList([10, 20,30,40]));
+
+//write a function to convert a list to array
+
+function listToArray(list){
+console.log('in function');
+  console.log(list); 
+arr = [list.value];
+  console.log('rest.value'+list.rest.value);
+  console.log('rest.rest.value'+list.rest.rest.value);
+ console.log('array:'+ arr); 
+  list1= list;
+while(list1.rest !== null){
+    arr.push(list1.rest.value);
+   list1.rest = list1.rest.rest;
+    console.log('in loop'+ list1.rest);
+    
+}
+console.log('returningarray'+arr);
+ return arr;    
+}     
+      
+//console.log(arrayToList([10, 20,30,40]));
+// → {value: 10, rest: {value: 20, rest: null}}
+console.log(listToArray(arrayToList([10, 20, 30,40])));
+
+
